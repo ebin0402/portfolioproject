c60715091c3fd3db9834ab9ce83068cc02a9f73f
ebin0402 <ebin0402@gmail.com>
2020-03-22 13:06:36 +0530

    commiting the changes made for jobs

--- ebinportfolio/settings.py
+++ ebinportfolio/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'ebinportfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ebinportfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'Ebincoder@5225',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
